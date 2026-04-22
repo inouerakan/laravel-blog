f5b20783573745f2ea2a6e191406cf36f565d6ae
inouerakan <rakanprojek@gmail.com>
2026-04-22 16:34:39 +0700

    responsive: mobile fix

--- src/components/BottomRight.jsx
+++ src/components/BottomRight.jsx
@@ -8,7 +8,7 @@ export default function BottomRight() {
     return (
         <div className='section-containers md:w-[80svw] w-svw md:h-[75svh] h-svh absolute bottom-0 right-0'>
             <div className='absolute md:bottom-[10%] md:left-[2%] md:top-auto top-[5%] left-[10%] rotate-5'><Book bgColor={'#7BAFC4'} title={'Blade Views'} url={'https://medium.com/@rakanshaka3/proyek-laravel-blade-views-0a72c2b0dac2'} /></div>
-            <div className='absolute md:top-0 md:right-[10.5%] md:bottom-auto right-[10%] bottom-[5%] -rotate-7'><Book bgColor={'#1A2F4A'} title={'Routes'} url={''} /></div>
+            <div className='absolute md:top-0 md:right-[10.5%] md:bottom-auto right-[10%] bottom-[5%] -rotate-7'><Book bgColor={'#1A2F4A'} title={'Routes'} url={'https://medium.com/@rakanshaka3/proyek-laravel-routes-a36ff6b78bbc'} /></div>
             <div className='absolute md:top-[4%] md:bottom-auto md:left-0 md:right-auto right-[10%] top-[5%] rotate-3'><Polaroid title={'井上和'} long={true} format={'jpg'} /></div>
             <div className='absolute md:bottom-[15%] md:left-[21.5%] md:top-auto md:right-auto top-[27.5%] right-[9%] -rotate-4'><Polaroid title={'インディゴ地平線'} format={'jpg'} /></div>
             <div className='absolute md:bottom-[11%] md:right-[7%] md:left-auto left-[10%] bottom-[30%] rotate-4'><Polaroid title={'ハチミツ'} long={false} format={'jpg'} /></div>
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -2,7 +2,7 @@ export default function Book({bgColor, title, url}) {
     const base = import.meta.env.BASE_URL;
 
     return (
-        <div style={{backgroundColor: bgColor}} className={`lg:w-37.5 md:w-28.75 w-20 lg:h-56.25 md:h-43 h-30 lg:text-xl md:text-lg text-base flex md:gap-2 gap-1 md:p-2 p-1 text-light rounded-xl shadow-2xl break-all`} onClick={() => window.open(url)}>
+        <div style={{backgroundColor: bgColor}} className={`lg:w-37.5 md:w-28.75 w-20 lg:h-56.25 md:h-43 h-30 lg:text-xl md:text-lg text-base flex md:gap-2 gap-1 md:p-2 p-1 text-light rounded-xl shadow-2xl break-all`} onClick={() => window.open(url)} ontouchstart={() => window.open(url)}>
             <div style={{backgroundColor: bgColor}} className={`w-1 h-full brightness-95 self-center`}></div>
             <p>{title}</p>
             <p style={{color: bgColor}} className="absolute md:bottom-2 bottom-1 md:left-5 left-2.5 md:text-sm text-xs brightness-85">Baca lebih lanjut</p>
